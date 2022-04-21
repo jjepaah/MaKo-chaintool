b1e35cc7c75634947abb6f0cdc2956043e63e11d
jjepaah <jjepaah@gmail.com>
2022-04-21 21:52:51 +0300

    tunnin bugitestaus yhen sanan takia

--- data/chaintool.js
+++ data/chaintool.js
@@ -1,12 +1,26 @@
 
-
-
 function myFunction() {
-    //document.getElementById("demo").innerHTML = "Hello!";
-    fetch("data/sedis-delta-data.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => console.log(data));
+    let pituus = document.getElementById("pituus").value;
+    
 
-  }
+    
+
+    
+    for(i in sedisData){
+      let jako = parseFloat(document.getElementById("jako").value);
+
+      let muunnos = sedisData[i].jako.replaceAll(",", ".");
+      let sedisJako = parseFloat(muunnos);
+
+      
+
+      if(jako == sedisJako){
+        document.getElementById("title").innerHTML = "Ketju on " + sedisData[i].iso;
+        break;
+      } else {
+        document.getElementById("title").innerHTML = "Sopivaa ketjua ei löytynyt";
+        console.log("Haku: ", jako, " / Vertaus: ", sedisJako);
+      }
+    }
+
+}
