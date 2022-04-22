309b33259c0b993566fcb7b25980b2a1b937c487
jjepaah <jjepaah@gmail.com>
2022-04-22 14:43:47 +0300

    lisätty toiminto joka hakee annetun arvon lähimpänä olevan ketjun taulukosta

--- data/chaintool.js
+++ data/chaintool.js
@@ -2,25 +2,44 @@
 function myFunction() {
     let pituus = document.getElementById("pituus").value;
     
+    let jako = parseFloat(document.getElementById("jako").value);
 
-    
+    document.getElementById("title").innerHTML = "Ketju on " + sedisData[nearest(jako)].iso;
 
-    
+    /*
     for(i in sedisData){
       let jako = parseFloat(document.getElementById("jako").value);
 
       let muunnos = sedisData[i].jako.replaceAll(",", ".");
       let sedisJako = parseFloat(muunnos);
 
-      
+      console.log(nearest(jako));
 
       if(jako == sedisJako){
         document.getElementById("title").innerHTML = "Ketju on " + sedisData[i].iso;
         break;
       } else {
         document.getElementById("title").innerHTML = "Sopivaa ketjua ei löytynyt";
-        console.log("Haku: ", jako, " / Vertaus: ", sedisJako);
+        //console.log("Haku: ", jako, " / Vertaus: ", sedisJako);
       }
     }
+    */
 
+}
+
+function nearest(sValue) {
+  let comp = 100;
+  let chain;
+  for(i in sedisData){
+    let b = sedisData[i].jako.replaceAll(",", ".");
+    let a = Math.abs(sValue - b);
+    //console.log(sedisData[i].jako);
+    //console.log(a);
+    if(a < comp){
+      chain = i;
+      comp = a;
+    }
+  }
+  //console.log("Chain: ", chain);
+  return chain;
 }
